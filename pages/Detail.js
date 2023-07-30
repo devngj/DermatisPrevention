a7ccfd97c807d3bbfec7ecb04407ae51cc986753
JinGee <jingee.kim9@gmail.com>
2023-07-30 15:57:10 +0900

    dashboard

--- pages/Detail.js
+++ pages/Detail.js
@@ -6,7 +6,7 @@ import { LineChart } from "react-native-chart-kit";
 import { Icon } from "@rneui/themed";
 
 const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "June"],
     datasets: [
         {
             data: [
@@ -34,10 +34,21 @@ const data = {
 export default function Detail() {
 
     const [isSelected, setSelection] = useState(false);
+    const [average, setAverage] = useState(10);
+    const [lastWeekAverage, setLastWeekAverage] = useState(6);
+    const [min, setMin] = useState(4);
+    const [lastWeekMin, setLastWeekMin] = useState(8);
+    const [max, setMax] = useState(15);
+    const [lastWeekMax] = useState(23);
+    const [activated, setActivated] = useState(1);
+    const [lastWeekActivated, setLastWeekActivated] = useState(2);
 
     return (
         <View
-            style={StyleSheet.container}
+            style={{
+                flex: 1,
+                backgroundColor: '#f7f7f7'
+            }}
         >
             <Text
                 style={{
@@ -45,17 +56,18 @@ export default function Detail() {
                     fontSize: hp(4),
                     fontWeight: 'bold',
                     marginTop: hp(8),
-                    marginLeft: hp(2)
+                    marginLeft: hp(3)
                 }}
             >
                 Sensor1
             </Text>
             <View style={{
                 flexDirection: 'row',
-                justifyContent: 'space-around'
+                justifyContent: 'space-around',
+                marginTop: hp(2)
             }}>
                 <View style={{
-                    backgroundColor: '#e9e4d8',
+                    backgroundColor: '#fff',
                     height: hp(15),
                     borderRadius: hp(2),
                     width: wp(40)
@@ -79,7 +91,7 @@ export default function Detail() {
                             fontWeight: 'bold'
                         }}
                     >
-                            10
+                            {average}
                     </Text>
                     <View
                         style={{
@@ -91,22 +103,26 @@ export default function Detail() {
                     >
                         <Icon 
                             type="ionicon"
-                            name="arrow-up-outline"
+                            name={(average - lastWeekAverage) > 0 ? "arrow-up-outline" : "arrow-down-outline"}
                             size={hp(2)}
-                            color={'#71d5aa'}
+                            color={(average - lastWeekAverage) > 0 ? '#71d5aa' : '#e65c6d'}
                             style={{
-                                backgroundColor: '#c1ecd9',
+                                backgroundColor: (average - lastWeekAverage) > 0 ? '#c1ecd9' : '#f4b8bf',
                                 borderRadius: 100
                             }}
                         />
                         <Text
                             style={{
                                 marginLeft: hp(1),
-                                color: '#71d5aa',
+                                color: (average - lastWeekAverage) > 0 ? '#71d5aa' : '#e65c6d',
                                 fontSize: hp(1.5)
                             }}
                         >
-                            +12.45%
+                            {(average - lastWeekAverage) > 0 ? 
+                            '+' + (((average-lastWeekAverage)/lastWeekAverage) * 100).toFixed(2) + '%'
+                            :
+                            (((average-lastWeekAverage)/lastWeekAverage) * 100).toFixed(2) + '%'
+                            }
                         </Text>
                     </View>
                     <Text
@@ -120,7 +136,7 @@ export default function Detail() {
                     </Text>
                 </View>
                 <View style={{
-                    backgroundColor: '#e9e4d8',
+                    backgroundColor: '#fff',
                     height: hp(15),
                     borderRadius: hp(2),
                     width: wp(40)
@@ -133,7 +149,7 @@ export default function Detail() {
                             marginTop: hp(2)
                         }}
                     >
-                        Min / Max
+                        Min
                     </Text>
                     <Text
                         style={{
@@ -144,39 +160,233 @@ export default function Detail() {
                             fontWeight: 'bold'
                         }}
                     >
-                            10
+                            {min}
+                    </Text>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            marginLeft: hp(2),
+                            marginTop: hp(1)
+                        }}
+                    >
+                        <Icon 
+                            type="ionicon"
+                            name={(min - lastWeekMin) > 0 ? "arrow-up-outline" : "arrow-down-outline"}
+                            size={hp(2)}
+                            color={(min - lastWeekMin) > 0 ? '#71d5aa' : '#e65c6d'}
+                            style={{
+                                backgroundColor: (min - lastWeekMin) > 0 ? '#c1ecd9' : '#f4b8bf',
+                                borderRadius: 100
+                            }}
+                        />
+                        <Text
+                            style={{
+                                marginLeft: hp(1),
+                                color: (min - lastWeekMin) > 0 ? '#71d5aa' : '#e65c6d',
+                                fontSize: hp(1.5)
+                            }}
+                        >
+                            {(min - lastWeekMin) > 0 ? 
+                            '+' + (((min-lastWeekMin)/lastWeekMin) * 100).toFixed(2) + '%'
+                            :
+                            (((min-lastWeekMin)/lastWeekMin) * 100).toFixed(2) + '%'
+                            }
+                        </Text>
+                    </View>
+                    <Text
+                        style={{
+                            marginLeft: hp(2),
+                            fontSize: hp(1.5),
+                            color: '#bcbcbc'
+                        }}
+                    >
+                        Than Last Week
+                    </Text>
+                </View>
+            </View>
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                marginTop: hp(2)
+            }}>
+                <View style={{
+                    backgroundColor: '#fff',
+                    height: hp(15),
+                    borderRadius: hp(2),
+                    width: wp(40)
+                }}>
+                    <Text
+                        style={{
+                            color: 'black',
+                            fontSize: hp(2),
+                            marginLeft: hp(2),
+                            marginTop: hp(2)
+                        }}
+                    >
+                        Max
+                    </Text>
+                    <Text
+                        style={{
+                            color: 'black',
+                            fontSize: hp(3),
+                            marginTop: hp(1),
+                            marginLeft: hp(2),
+                            fontWeight: 'bold'
+                        }}
+                    >
+                            {max}
+                    </Text>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            marginLeft: hp(2),
+                            marginTop: hp(1)
+                        }}
+                    >
+                        <Icon 
+                            type="ionicon"
+                            name={(max - lastWeekMax) > 0 ? "arrow-up-outline" : "arrow-down-outline"}
+                            size={hp(2)}
+                            color={(max - lastWeekMax) > 0 ? '#71d5aa' : '#e65c6d'}
+                            style={{
+                                backgroundColor: (max - lastWeekMax) > 0 ? '#c1ecd9' : '#f4b8bf',
+                                borderRadius: 100
+                            }}
+                        />
+                        <Text
+                            style={{
+                                marginLeft: hp(1),
+                                color: (max - lastWeekMax) > 0 ? '#71d5aa' : '#e65c6d',
+                                fontSize: hp(1.5)
+                            }}
+                        >
+                            {(max - lastWeekMax) > 0 ? 
+                            '+' + (((max-lastWeekMax)/lastWeekMax) * 100).toFixed(2) + '%'
+                            :
+                            (((max-lastWeekMax)/lastWeekMax) * 100).toFixed(2) + '%'
+                            }
+                        </Text>
+                    </View>
+                    <Text
+                        style={{
+                            marginLeft: hp(2),
+                            fontSize: hp(1.5),
+                            color: '#bcbcbc'
+                        }}
+                    >
+                        Than Last Week
+                    </Text>
+                </View>
+                <View style={{
+                    backgroundColor: '#fff',
+                    height: hp(15),
+                    borderRadius: hp(2),
+                    width: wp(40)
+                }}>
+                    <Text
+                        style={{
+                            color: 'black',
+                            fontSize: hp(2),
+                            marginLeft: hp(2),
+                            marginTop: hp(2)
+                        }}
+                    >
+                        Time
+                    </Text>
+                    <Text
+                        style={{
+                            color: 'black',
+                            fontSize: hp(3),
+                            marginTop: hp(1),
+                            marginLeft: hp(2),
+                            fontWeight: 'bold'
+                        }}
+                    >
+                            {activated}
+                    </Text>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            marginLeft: hp(2),
+                            marginTop: hp(1)
+                        }}
+                    >
+                        <Icon 
+                            type="ionicon"
+                            name={(activated - lastWeekActivated) > 0 ? "arrow-up-outline" : "arrow-down-outline"}
+                            size={hp(2)}
+                            color={(activated - lastWeekActivated) > 0 ? '#71d5aa' : '#e65c6d'}
+                            style={{
+                                backgroundColor: (activated - lastWeekActivated) > 0 ? '#c1ecd9' : '#f4b8bf',
+                                borderRadius: 100
+                            }}
+                        />
+                        <Text
+                            style={{
+                                marginLeft: hp(1),
+                                color: (activated - lastWeekActivated) > 0 ? '#71d5aa' : '#e65c6d',
+                                fontSize: hp(1.5)
+                            }}
+                        >
+                            {(activated - lastWeekActivated) > 0 ? 
+                            '+' + (((activated-lastWeekActivated)/lastWeekActivated) * 100).toFixed(2) + '%'
+                            :
+                            (((activated-lastWeekActivated)/lastWeekActivated) * 100).toFixed(2) + '%'
+                            }
+                        </Text>
+                    </View>
+                    <Text
+                        style={{
+                            marginLeft: hp(2),
+                            fontSize: hp(1.5),
+                            color: '#bcbcbc'
+                        }}
+                    >
+                        Than Last Week
                     </Text>
                 </View>
             </View>
             <View
                 style={{
-                    alignItems: 'center'
+                    backgroundColor: 'white',
+                    paddingVertical: hp(3),
+                    width: wp(90),
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    borderRadius: 16,
+                    marginTop: hp(2)
                 }}
             >
+                <Text
+                    style={{
+                        fontWeight: 'bold',
+                        fontSize: hp(2),
+                        marginLeft: hp(2)
+                    }}
+                >
+                    Overview
+                </Text>
                 <LineChart
                     data={data}
                     width={wp(90)} // from react-native
                     height={220}
-                    yAxisLabel="$"
-                    yAxisSuffix="k"
                     yAxisInterval={1} // optional, defaults to 1
+                    withInnerLines={false}
+                    withOuterLines={false}
                     chartConfig={{
-                        backgroundColor: "#e26a00",
-                        backgroundGradientFrom: "#fb8c00",
-                        backgroundGradientTo: "#ffa726",
+                        backgroundColor: "#fff",
+                        backgroundGradientFrom: "#fff",
+                        backgroundGradientTo: "#fff",
                         decimalPlaces: 2, // optional, defaults to 2dp
-                        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
                         style: {
                             borderRadius: 16,
-                        },
-                        propsForDots: {
-                            r: "6",
-                            strokeWidth: "2",
-                            stroke: "#ffa726"
                         }
                     }}
-                    bezier
                     style={{
                         marginVertical: 8,
                         borderRadius: 16
@@ -184,15 +394,6 @@ export default function Detail() {
                     resizeMode="contain"
                 />
             </View>
-            <View style={styles.checkboxContainer}>
-                <Checkbox
-                    value={isSelected}
-                    onValueChange={setSelection}
-                    style={styles.checkbox}
-                />
-                <Text style={styles.label}>Do you like React Native?</Text>
-            </View>
-            <Text>Is CheckBox selected: {isSelected ? '👍' : '👎'}</Text>
         </View>
     )
 }
